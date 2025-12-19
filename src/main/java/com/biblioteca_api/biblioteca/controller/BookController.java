632d4b327a9bca3d812976061e676e853908a3f9
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2025-12-19 15:11:57 -0400

    endpoint de pegar livro pelo id

--- src/main/java/com/biblioteca_api/biblioteca/controller/BookController.java
+++ src/main/java/com/biblioteca_api/biblioteca/controller/BookController.java
@@ -1,8 +1,12 @@
 package com.biblioteca_api.biblioteca.controller;
 
 
+import com.biblioteca_api.biblioteca.dto.BookResponseDTO;
+import com.biblioteca_api.biblioteca.entities.Book;
 import com.biblioteca_api.biblioteca.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookService bookService;
+
+    // (GET) - Livro pelo ID.
+    public ResponseEntity<BookResponseDTO> getBookById(@PathVariable Long id){
+        Book book = bookService.getBookById(id);
+        BookResponseDTO response = new BookResponseDTO(book);
+
+        return ResponseEntity.ok(response);
+    }
 
 }
