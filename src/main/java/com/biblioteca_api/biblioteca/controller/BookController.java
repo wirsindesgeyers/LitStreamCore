2a5b50a433e523487be603779365c17dcc311625
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2025-12-19 15:20:00 -0400

    endpoint de pegar todos os livros

--- src/main/java/com/biblioteca_api/biblioteca/controller/BookController.java
+++ src/main/java/com/biblioteca_api/biblioteca/controller/BookController.java
@@ -6,9 +6,12 @@ import com.biblioteca_api.biblioteca.entities.Book;
 import com.biblioteca_api.biblioteca.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -18,10 +21,20 @@ public class BookController {
     private final BookService bookService;
 
     // (GET) - Livro pelo ID.
+    @GetMapping("{/id}")
     public ResponseEntity<BookResponseDTO> getBookById(@PathVariable Long id){
         Book book = bookService.getBookById(id);
         BookResponseDTO response = new BookResponseDTO(book);
+        return ResponseEntity.ok(response);
+    }
 
+    //(GET) - Todos os livros
+    @GetMapping
+    public ResponseEntity<List<BookResponseDTO>> getAllBooks (){
+        List<Book> books = bookService.getAllBooks();
+        List<BookResponseDTO> response = books.stream()
+                .map(BookResponseDTO::new)
+                .toList();
         return ResponseEntity.ok(response);
     }
 
